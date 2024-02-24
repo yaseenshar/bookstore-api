9c545ffefb67f77faa8e1c050c0856cc9cbc55fc
yaseen <yaseenshar50@gmail.com>
2024-02-24 11:00:21 +0100

    First Commit Structure of Project

---
+++ src/main/java/com/m3sysoft/bookstore/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.m3sysoft.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
